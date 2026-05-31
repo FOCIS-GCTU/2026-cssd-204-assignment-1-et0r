26059cfa6a808856078c30ad3c03e24c76ee6005
iametor <wisetexh@gmail.com>
2026-05-31 21:06:56 +0000

    added header comments

--- initials.py
+++ initials.py
@@ -1,3 +1,15 @@
+# File: WEA.py
+# Description: a program that prints my initials in massive block letters using 10prints stament written in a function
+# Assignment Number: 1
+#
+# Name: Wisdom Alornyo
+# STUDENT ID: 2425403280
+# Email: 2425403280@gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Wisdom Alornyo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():  # I defined the function it contains all the staments to print my initials in massive block as directed by the instructions.
   pass #code goes here

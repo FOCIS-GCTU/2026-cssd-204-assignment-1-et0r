2a9ef370197258a8ddd8b6097541e82a8ff2612a
Wisdom Alornyo <284327228+et0r@users.noreply.github.com>
2026-05-31 18:37:10 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,19 @@
 
-def main():
+def main():  # I defined the function it contains all the staments to print my initials in massive block as directed by the instructions.
   pass #code goes here
+  print()
+    print("...WEA")
+    print("")
+    print("...WW...WW...WW........EEEEEEEEEEEE..........AAAAAAAA.......")
+    print("...WW...WW...WW........EEEEEEEEEEEE.........AAAAAAAAAA......")
+    print("...WW...WW...WW........EEE.................AAAA....AAAA.....")
+    print("...WW..WWWW..WW........EEE.................AAAA....AAAA.....")
+    print("...WW..WWWW..WW........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...WWWWW..WW.WW........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...WWWW....WWWW........EEE.................AAAA....AAAA.....")
+    print("...WWWW....WWWW........EEE.................AAAA....AAAA.....")
+    print("...WWWW....WWWW...**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print("...WWWW....WWWW...**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print()
 
-
-
-main()
+main() # I called the function to print my initials in block

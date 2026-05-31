d22fadd1a1f166b07fd391615de47213f3315ba8
iametor <wisetexh@gmail.com>
2026-05-31 16:29:45 +0000

    Completed assignment 1

---
+++ WEA.py
@@ -0,0 +1,30 @@
+# File: WEA.py
+# Description: a program that prints my initials in massive block letters using 10prints stament written in a function
+# Assignment Number: 1
+#
+# Name: Wisdom Alornyo
+# STUDENT ID: 2425403280
+# Email: 2425403280@gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Wisdom Alornyo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main(): # I defined the function it contains all the staments to print my initials in massive block as directed by the instructions.
+    print()
+    print("...WEA")
+    print("")
+    print("...MM...MM...MM........EEEEEEEEEEEE..........AAAAAAAA.......")
+    print("...MM...MM...MM........EEEEEEEEEEEE.........AAAAAAAAAA......")
+    print("...MM...MM...MM........EEE.................AAAA....AAAA.....")
+    print("...MM..MMMM..MM........EEE.................AAAA....AAAA.....")
+    print("...MM..MMMM..MM........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...MM.MM..MM.MM........EEEEEEEEEEEE........AAAADDDDAAAA.....")
+    print("...MM.MM..MM.MM........EEE.................AAAA....AAAA.....")
+    print("...MM.MM..MM.MM........EEE.................AAAA....AAAA.....")
+    print("...MMMM....MMMM...**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print("...MMMM....MMMM...**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print()
+    
+main() # I called the function to print my initials in block
